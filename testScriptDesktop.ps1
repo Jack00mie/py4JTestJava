928d60aa97894a6008fd497c5e5ec09fe6ced181
LeonP <leon.pueschel@gmx.de>
2025-04-08 09:47:52 +0200

    add test script for desktop pc

---
+++ testScriptDesktop.ps1
@@ -0,0 +1,56 @@
+﻿Write-Host "Begin testing..."
+
+# Root dir of the java and python app
+$javaRootDir = "C:\Users\Leon Püschel\IdeaProjects\py4JTestJava"
+$pythonRootDir = "C:\Users\Leon Püschel\PycharmProjects\py4jTest"
+# The size of the observationVector you want to test with. More means more data being send.
+$observationVectorSize = "200"
+# increase for slower Systems
+$waitTimeFactor = 1
+
+# Activate venv
+."$($pythonRootDir)\.venv\Scripts\activate.ps1"
+
+for ($i = 0; $i -lt 20; $i++) {
+    Write-Host "$($i). test round"
+
+    $pythonDir = ""
+    $javaDir = ""
+    # flip flop beetween http and py4J test
+    if ($i % 2 -eq 0) {
+        # Http test
+        Write-Host "Http test round..."
+        $pythonDir = "\httpTest\httpEnvironment.py"
+        $javaDir = "\out\artifacts\httpTestJava_jar\httpTestJava.jar"
+
+    } else {
+        # py4J test
+        Write-Output "Py4J test round.."
+        $pythonDir = "\py4JTest\main.py"
+        $javaDir = "\out\artifacts\py4JTestJava_jar\py4JTestJava.jar"
+    }
+
+    # Start Python process and capture its process object
+    $pythonProcess = Start-Process -FilePath python `
+        -ArgumentList "`"$($pythonRootDir)$($pythonDir)`"", "$($observationVectorSize)" `
+        -PassThru
+
+    Start-Sleep -Seconds (5 * $waitTimeFactor)
+
+    # Start Java process and capture its process object
+    $javaProcess = Start-Process -FilePath java `
+        -ArgumentList "-jar", "`"$($javaRootDir)$($javaDir)`"", "$($observationVectorSize)" `
+        -PassThru
+   
+    Start-Sleep -Seconds 1.5
+
+    # Wait until both processes are no longer running
+    Wait-Process -Id $pythonProcess.Id, $javaProcess.Id
+
+    Start-Sleep -Seconds (1.5 * $waitTimeFactor)
+}
+
+
+Write-Host "Test complete."
+# Deactivate venv
+deactivate
